3aed25e5db6e655c4620261270ac5eba3836870d
wsqviva <wsq19850829@gmail.com>
2016-11-24 14:40:54 +0800

    temp commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 import Vue from 'vue'
 import _ from 'lodash/fp'
-import * as a from './a'
 
-let b = _.map((item) => {
-  return item + 1
-})
-b([1, 2, 3])
-
-
-// console.log(Vue)
-// console.log(a.abc())
